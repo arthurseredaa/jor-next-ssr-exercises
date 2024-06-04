84d4a64bfbb96a8812abaa678ab8e06f0b2d7394
arthursereda <arthur.sereda@promova.com>
2024-06-04 23:52:47 +0300

    fix: hydration mismatching issue for Clock.js client component

--- src/app/exercises/01-clock/Clock.js
+++ src/app/exercises/01-clock/Clock.js
@@ -3,21 +3,25 @@ import React from 'react';
 import format from 'date-fns/format';
 
 function Clock() {
-  const [time, setTime] = React.useState(new Date());
+    const [time, setTime] = React.useState(null);
 
-  React.useEffect(() => {
-    const intervalId = window.setInterval(() => {
-      setTime(new Date());
-    }, 50);
+    React.useEffect(() => {
+        const intervalId = window.setInterval(() => {
+            setTime(new Date());
+        }, 50);
 
-    return () => {
-      window.clearInterval(intervalId);
-    };
-  }, []);
+        return () => {
+            window.clearInterval(intervalId);
+        };
+    }, []);
 
-  return (
-    <p className="clock">{format(time, 'hh:mm:ss.S a')}</p>
-  );
+    return (
+        // ERROR: on server it will render <p></p>, but on client after initial render it will be <p><span>Please wait...</span></p> - different layouts
+        // <p className="clock">{time ? format(time, 'hh:mm:ss.S a') : <span>Please wait...</span>}</p>
+        <p className="clock">{time ? format(time, 'hh:mm:ss.S a') : '———'}</p>
+        // FIX: second possible fix: <p className="clock" suppressHydrationWarning>{format(time, 'hh:mm:ss.S a')}</p> (only for one level deep, no nested elements)
+        // INFO: https://legacy.reactjs.org/docs/dom-elements.html#suppresshydrationwarning
+    );
 }
 
 export default Clock;
